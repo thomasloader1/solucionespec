e4e7bc1ccd1036857cdb1e4277ac03b5b7d2fe7c
Tomas Gomez <talk.tomas@hotmail.com>
2020-07-06 16:46:50 -0300

    Generacion de mixins y arreglos con el proyecto, revisar navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Main.js running")
+$(document).ready(function () {
+    $('.first-button').on('click', function () {
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+        $('.animated-icon3').toggleClass('open');
+    });
+});
